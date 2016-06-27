12ead3ccea0f339ea8f53f4b1b7e600131985bc0
Caitlin <chayles.916@gmail.com>
2016-06-27 16:07:35 +0200

    submission!

---
+++ HelloWorld.js
@@ -0,0 +1,36 @@
+var continent = prompt ("Which continent have you travelled to the least?").toLowerCase();
+
+switch (continent) {
+	case 'Australia':
+		var str = "All about Australia!";
+		var result = str.link ("https://www.lonelyplanet.com/australia");
+		console.log("We haven't been to Australia, but you can check this out:" str);
+		break;
+	case 'Antarctica':
+		console.log("No surprise there! Maybe you should look around the site for something a little easier to get to.");
+		break;
+	default:
+		var pickCity = prompt ("Which city would you most like to visit in" + " " + continent + "?").toLowerCase();
+		if (pickCity === ("banff" || "seattle" || "belmopan" || "calgary" || "san francisco" || "belize city" || "golden" || "portland" || "nassau" ||
+			|| "kelowna" || "las angeles" || "bridgetown" || "montreal" || "anaheim" || "tikal" || "toronto" || "scottsdale" || "vancouver"
+			|| "sedona" || "victoria" || "albuquerque" || "las vegas" || "park city" || "salt lake city" || "sun valley" || "big sky"
+			|| "jackson hole" || "telluride" || "dallas" || "rapid city" || "kadoka" || "cody" || "chicago" || "philadelphia" || "jersey city"	
+			|| "newark" || "new york city" || "new haven" || "baltimore" || "nashville" || "miami" || "orlando" || "tampa" || "charlotte"	
+			|| "wilmington" || "raleigh" || "maui" ||"lanai")) {
+				console.log("Looks like we have that city! Scroll down to the first section of the page to check it out.");
+		}
+		if else (pickCity === ('quito' || 'galapagos' || 'buenos aires')){
+				console.log("Looks like we have that city! Scroll down to the second section of the page to check it out.");
+		}
+		if else (pickCity ===('york' || 'rome' || 'paris' || 'palermo' || 'nice' || 'london' || 'istanbul'
+								|| 'edinburgh' || 'birmingham' || 'bilbao' || 'barcelona')){
+				console.log("Looks like we have that city! Scroll down to the third section of the page to check it out.");
+		}
+		if else (pickCity ===('maun' || 'cape town' || 'victoria falls')){
+				console.log("Looks like we have that city! Scroll down to the fourth section of the page to check it out.");
+		}
+		if else (pickCity === ('bodrum' || 'cappadocia')) {
+				console.log("Looks like we have that city! Scroll down to the fourth section of the page to check it out.");
+		}
+		else {console.log("Hmmm, we can't seem to find that. Maybe look in our footer to find a good link!")};
+};
