d3d52b76586691f7c97678dfc710d664a9afc893
Caitlin <chayles.916@gmail.com>
2016-07-01 13:18:04 +0200

    My Hello World Commit

--- HelloWorld.js
+++ HelloWorld.js
@@ -11,12 +11,12 @@ switch (continent) {
 		break;
 	default:
 		var pickCity = prompt ("Which city would you most like to visit in" + " " + continent + "?").toLowerCase();
-		if (pickCity === ("banff" || "seattle" || "belmopan" || "calgary" || "san francisco" || "belize city" || "golden" || "portland" || "nassau" ||
-			|| "kelowna" || "las angeles" || "bridgetown" || "montreal" || "anaheim" || "tikal" || "toronto" || "scottsdale" || "vancouver"
+		if (pickCity === ("banff" || "seattle" || "calgary" || "san francisco" || "belize city" || "golden" || "portland" || "nassau" ||
+			|| "kelowna" || "los angeles" || "bridgetown" || "montreal" || "anaheim" || "tikal" || "toronto" || "scottsdale" || "vancouver"
 			|| "sedona" || "victoria" || "albuquerque" || "las vegas" || "park city" || "salt lake city" || "sun valley" || "big sky"
-			|| "jackson hole" || "telluride" || "dallas" || "rapid city" || "kadoka" || "cody" || "chicago" || "philadelphia" || "jersey city"	
-			|| "newark" || "new york city" || "new haven" || "baltimore" || "nashville" || "miami" || "orlando" || "tampa" || "charlotte"	
-			|| "wilmington" || "raleigh" || "maui" ||"lanai")) {
+			|| "jackson hole" || "telluride" || "dallas" || "rapid city" || "cody" || "chicago" || "philadelphia" || "jersey city"	
+			|| "new york city" || "new haven" || "baltimore" || "nashville" || "orlando" || "charlotte"	
+			|| "maui" ||"lanai")) {
 				console.log("Looks like we have that city! Scroll down to the first section of the page to check it out.");
 		}
 		if else (pickCity === ('quito' || 'galapagos' || 'buenos aires')){
@@ -30,7 +30,7 @@ switch (continent) {
 				console.log("Looks like we have that city! Scroll down to the fourth section of the page to check it out.");
 		}
 		if else (pickCity === ('bodrum' || 'cappadocia')) {
-				console.log("Looks like we have that city! Scroll down to the fourth section of the page to check it out.");
+				console.log("Looks like we have that city! Scroll down to the fifth section of the page to check it out.");
 		}
-		else {console.log("Hmmm, we can't seem to find that. Maybe look in our footer to find a good link!")};
+		else {console.log("Hmmm, we can't seem to find that. Maybe look in our footer to find a good link for more info!")};
 };
