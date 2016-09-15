ebeca4791fdcf71a0f681f53847aa7da86b37f63
Caitlin Hayles <cjhayles93@gmail.com>
2016-09-15 12:29:43 -0400

    Updated Portfolio Page and added 30 before 30

--- HelloWorld.js
+++ HelloWorld.js
@@ -16,6 +16,9 @@ app.config(function($routeProvider){
 	}).when("/Travel",{
 		controller:"TravelCtrl",
 		templateUrl: "Templates/Travel.html"
+	}).when("/Thirty_Before_Thirty",{
+		controller:"ThirtyCtrl",
+		templateUrl: "Templates/Thirty.html"
 	})
 });
 
@@ -27,17 +30,73 @@ app.controller("HomeCtrl", function($scope){
 });
 
 app.controller("AboutMeCtrl", function($scope){
-
+	$scope.wantFooter = true;
 });
 
 app.controller("PortfolioCtrl", function($scope){
-	
+	$scope.Resume = false;
+	$scope.Guide = false;
+	$scope.Infographic = false;
+	$scope.Niccan = false;
+	$scope.Advertorial = false;
+	$scope.GPC = false;
+	$scope.Blog = false;
+	$scope.Passport = false;
+	$scope.Volleyball = false;
+
+	$scope.showResume = function(){
+		$scope.Resume = true;
+	}
+	$scope.showGuide = function(){
+		$scope.Guide = true;
+	}
+	$scope.showInfographic = function(){
+		$scope.Infographic = true;
+	}
+	$scope.showNiccan = function(){
+		$scope.Niccan = true;
+	}
+	$scope.showAdvertorial = function(){
+		$scope.Advertorial = true;
+	}
+	$scope.showGPC = function(){
+		$scope.GPC = true;
+	}
+	$scope.showBlog = function(){
+		$scope.Blog = true;
+	}
+	$scope.showFacebookPost = function(){
+		$scope.Post = true;
+	}
+	$scope.showVolleyball = function(){
+		$scope.Volleyball = true;
+	}
+
+	$scope.closePortfolioItem = function(){
+		$scope.Resume = false;
+		$scope.Guide = false;
+		$scope.Infographic = false;
+		$scope.Niccan = false;
+		$scope.Advertorial = false;
+		$scope.GPC = false;
+		$scope.Blog = false;
+		$scope.Post = false;
+		$scope.Volleyball = false;
+	}
 })
 
 app.controller("InterestsCtrl", function($scope){
-	
+	$scope.wantFooter = true;
 })
 
 app.controller("TravelCtrl", function($scope){
-	
+	$scope.wantFooter = true;
+})
+
+app.controller("ThirtyCtrl", function($scope){
+	$scope.wantFooter = false;
+	$scope.AfricaPostTab = false;
+	$scope.AfricaPost = function(){
+		$scope.AfricaPostTab = true;
+	}
 })
